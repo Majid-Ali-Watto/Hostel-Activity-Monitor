acf08dc3fb9ba553f6332db87b8eb8767fba919a
Majid Ali <majidaliqau@gmail.com>
2023-04-30 08:10:29 +0500

    almost same

--- components/SecuritySection.js
+++ components/SecuritySection.js
@@ -1,29 +1,29 @@
+/** @format */
+
 import React, { useState } from "react";
 import SList from "./search_list";
 import { styles } from "../assets/styles/securitysection";
 import { View, SafeAreaView } from "react-native";
 import LoginOrSignUp from "./generic_components/Login";
 export default function SecuritySection({ navigation }) {
-  const [, setUser] = useState("");
-  const [hideLogin, setHideLogin] = useState("none");
-  const [showLogin, setShowLogin] = useState("flex");
+	const [, setUser] = useState("");
+	const [hideLogin, setHideLogin] = useState("none");
+	const [showLogin, setShowLogin] = useState("flex");
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={{ flex: 1, display: showLogin }}>
-        <LoginOrSignUp
-          navigation={navigation}
-          userData={["security", "CNIC", 13, "Security Supervisor"]}
-          hideLogin={setHideLogin}
-          showLogin={setShowLogin}
-          setUserData={setUser}
-        />
-      </View>
-      <View style={{ flex: 1, display: hideLogin }}>
-        {hideLogin == "flex" && (
-          <SList navigation={navigation} userRole={"Security Supervisor"} />
-        )}
-      </View>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<View style={{ flex: 1, display: showLogin }}>
+				<LoginOrSignUp
+					navigation={navigation}
+					userData={["security", "CNIC", 13, "Security Supervisor"]}
+					hideLogin={setHideLogin}
+					showLogin={setShowLogin}
+					setUserData={setUser}
+				/>
+			</View>
+			<View style={{ flex: 1, display: hideLogin }}>
+				{hideLogin == "flex" && <SList navigation={navigation} userRole={"Security Supervisor"} />}
+			</View>
+		</SafeAreaView>
+	);
 }
--- components/renderStudentCard.js
+++ components/renderStudentCard.js
@@ -1,79 +1,67 @@
+/** @format */
+
 import React from "react";
 import { styles } from "../assets/styles/search_lists";
 import { Card } from "react-native-paper";
 import { View, Text, Image } from "react-native";
+
 import { HEIGHT, WIDTH } from "../Constants/GlobalWidthHeight";
 export default function renderStudentCard(
-  rollno,
-  cnic,
-  backgroundColor,
-  setSem,
-  semno,
-  setSearchTerm,
-  setUri,
-  sname,
-  image,
-  dname,
-  program,
-  statusText,
-  mStatusText,
-  showProfile,
-  titleFont,
-  rollNoFont,
-  programFont,
-  semNoFont,
-  departmentFont,
-  textColor
+	rollno,
+	cnic,
+	backgroundColor,
+	setSem,
+	semno,
+	setSearchTerm,
+	setUri,
+	sname,
+	image,
+	dname,
+	program,
+	statusText,
+	mStatusText,
+	showProfile,
+	titleFont,
+	rollNoFont,
+	programFont,
+	semNoFont,
+	departmentFont,
+	textColor
 ) {
-  return (
-    <Card
-      key={rollno + cnic}
-      style={[styles.card, backgroundColor]}
-      onPress={() => {
-        setSem(semno);
-        setSearchTerm(rollno);
-      }}
-      onLongPress={() => {
-        setUri([
-          sname,
-          image,
-          rollno,
-          dname,
-          program,
-          semno,
-          statusText,
-          mStatusText,
-        ]);
-        showProfile(true);
-      }}
-    >
-      <View style={{ flexDirection: "row" }}>
-        <View style={{ margin: 0, padding: 0, width: WIDTH * 0.15 }}>
-          <Image source={{ uri: image }} style={styles.img} />
-        </View>
-        <View>
-          <View style={[styles.data, { width: WIDTH * 0.7 }]}>
-            <View>
-              <Text style={[styles.title, titleFont, textColor]}>{sname}</Text>
-              <Text style={[styles.rollno, rollNoFont, textColor]}>
-                {rollno}
-              </Text>
-              <Text style={[styles.rollno, rollNoFont, textColor]}>{cnic}</Text>
-              <Text style={[styles.department, departmentFont, textColor]}>
-                {dname}
-              </Text>
-            </View>
-          </View>
-        </View>
-        <View style={{ margin: 0, padding: 0, width: WIDTH * 0.15 }}>
-          <Text style={[styles.program, programFont, textColor]}>
-            {program}
-          </Text>
-          <Text style={[styles.semno, semNoFont, textColor]}>{semno}</Text>
-        </View>
-      </View>
-      {/* <View style={{ flex: 1, justifyContent: "center" }}> */}
-      {/* <View style={[styles.cardsItems, { height: "70%" }]}>
+	return (
+		<Card
+			key={rollno + cnic}
+			style={[styles.card, backgroundColor]}
+			onPress={() => {
+				setSem(semno);
+				setSearchTerm(rollno);
+			}}
+			onLongPress={() => {
+				setUri([sname, image, rollno, dname, program, semno, statusText, mStatusText]);
+				showProfile(true);
+			}}
+		>
+			<View style={{ flexDirection: "row" }}>
+				<View style={{ margin: 0, padding: 0, width: WIDTH * 0.15 }}>
+					<Image source={{ uri: image }} style={styles.img} resizeMode="contain" />
+				</View>
+				<View>
+					<View style={[styles.data, { width: WIDTH * 0.7 }]}>
+						<View>
+							<Text style={[styles.title, titleFont, textColor]}>{sname}</Text>
+							<Text style={[styles.rollno, rollNoFont, textColor]}>{rollno}</Text>
+							<Text style={[styles.rollno, rollNoFont, textColor]}>{cnic}</Text>
+							<Text style={[styles.department, departmentFont, textColor]}>{dname}</Text>
+						</View>
+					</View>
+				</View>
+				<View style={{ margin: 0, padding: 0, width: WIDTH * 0.15 }}>
+					<Text style={[styles.program, programFont, textColor]}>{program}</Text>
+					<Text style={[styles.semno, semNoFont, textColor]}>{semno}</Text>
+				</View>
+			</View>
+			{/* <View style={{ flex: 1, justifyContent: "center" }}> */}
+			{/* <View style={[styles.cardsItems, { height: "70%" }]}>
         <View>
           <Image source={{ uri: image }} style={styles.img} />
         </View>
@@ -102,7 +90,7 @@ export default function renderStudentCard(
           {dname}
         </Text>
       </View> */}
-      {/* </View> */}
-    </Card>
-  );
+			{/* </View> */}
+		</Card>
+	);
 }
